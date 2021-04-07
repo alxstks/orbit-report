6d402c6c05a190154182eb1b9174c2f7c39b46b3
Your Name <you@example.com>
2021-04-07 16:32:37 -0500

    somehow got space debris warning working

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,6 +12,14 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean {
+        if(this.type.toLowerCase() === 'space debris'){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 
