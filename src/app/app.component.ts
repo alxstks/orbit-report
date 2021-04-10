b622358f49be935c09d8fadde435c1780cc088fe
Your Name <you@example.com>
2021-04-09 22:06:39 -0500

    working on search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,14 +39,34 @@ export class AppComponent {
    search(searchTerm: string): void {
     let matchingSatellites: Satellite[] = [];
     searchTerm = searchTerm.toLowerCase();
+    
+    //name
     for(let i=0; i < this.sourceList.length; i++) {
-       let name = this.sourceList[i].name.toLowerCase(); //Search type too, 
-       if (name.indexOf(searchTerm) >= 0) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
           matchingSatellites.push(this.sourceList[i]);
-       }
+      }
     }
+
+    //type
+    for(let i=0; i < this.sourceList.length; i++) {
+      let type = this.sourceList[i].type.toLowerCase();
+      if (type.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
+      }
+   }
+
+   //orbit type
+   for(let i=0; i < this.sourceList.length; i++) {
+      let orbitType = this.sourceList[i].orbitType.toLowerCase();        
+      if (orbitType.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
+      }
+   }
     // assign this.displayList to be the array of matching satellites
     // this will cause Angular to re-make the table, but now only containing matches
     this.displayList = matchingSatellites;
     }
+
+    
 }
